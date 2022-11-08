cee82205271b17a6bc97503e81c1cca82974b7b2
alexzhang1030 <1642114555@qq.com>
2022-11-08 17:39:16 +0800

    feat: finish

--- client/index.bash
+++ client/index.bash
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-index_all() {
+all() {
     http get localhost:8080/
 }
 
-index_all
+add() {
+    http post localhost:8080/ title="write Rust"
+}
+
+update() {
+    http put localhost:8080/1 --raw '{ "title": "write Rust updated", "completed": true }'
+}
+
+delete() {
+    http delete localhost:8080/1
+}
+
+read -p "type operator (all, add, update, delete): " list
+
+for name in $list; do
+    $name
+done
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,36 +1,115 @@
 use actix_web::{
-    get, post,
+    delete, get, post, put,
     web::{self, Data},
     App, HttpResponse, HttpServer, Responder,
 };
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::sync::{Mutex, MutexGuard};
 
-#[derive(Clone)]
 struct AppData {
-    todos: Vec<Todo>,
+    current_id: Mutex<i32>,
+    todos: Mutex<Vec<Todo>>,
 }
 
 #[derive(Serialize, Clone)]
 struct Todo {
-    id: u32,
+    id: i32,
     title: String,
     completed: bool,
 }
 
+#[derive(Deserialize)]
+struct AddTodo {
+    title: String,
+}
+
+#[derive(Deserialize)]
+struct UpdateParams {
+    id: i32,
+}
+
+#[derive(Deserialize)]
+struct UpdateTodo {
+    #[serde(default)]
+    title: String,
+    #[serde(default)]
+    completed: bool,
+}
+
 #[get("/")]
-async fn index(todos: Data<Vec<Todo>>) -> impl Responder {
-    HttpResponse::Ok().json(web::Json(todos))
+async fn index(data: Data<AppData>) -> impl Responder {
+    HttpResponse::Ok().json(web::Json(&data.todos))
+}
+
+#[post("/")]
+async fn insert(data: web::Data<AppData>, todo: web::Json<AddTodo>) -> impl Responder {
+    let mut current_id = data.current_id.lock().unwrap();
+    let mut todos = data.todos.lock().unwrap();
+    let todo = Todo {
+        id: current_id.clone(),
+        title: todo.title.clone(),
+        completed: false,
+    };
+    todos.push(todo.clone());
+    *current_id += 1;
+    HttpResponse::Created().json(web::Json(todo))
+}
+
+#[put("/{id}")]
+async fn update(
+    data: web::Data<AppData>,
+    params: web::Path<UpdateParams>,
+    update_data: web::Json<UpdateTodo>,
+) -> impl Responder {
+    let mut todos = data.todos.lock().unwrap();
+    let id = params.id;
+    let exist = exist_by_id(id, &todos);
+    match exist {
+        Some(i) => {
+            let todo = todos.get_mut(i).unwrap();
+            let updated_todo = Todo {
+                id: todo.id.clone(),
+                title: update_data.title.clone(),
+                completed: update_data.completed.clone(),
+            };
+            *todo = updated_todo.clone();
+            HttpResponse::Ok().json(web::Json(updated_todo))
+        }
+        _ => HttpResponse::NotFound().finish(),
+    }
+}
+
+#[delete("/{id}")]
+async fn remove(data: web::Data<AppData>, params: web::Path<UpdateParams>) -> impl Responder {
+    let mut todos = data.todos.lock().unwrap();
+    let id = params.id;
+    let exist = exist_by_id(id, &todos);
+    match exist {
+        Some(i) => {
+            todos.remove(i);
+            HttpResponse::Ok().body("Todo deleted")
+        }
+        None => HttpResponse::NotFound().body("Todo not found"),
+    }
+}
+
+fn exist_by_id(id: i32, state: &MutexGuard<Vec<Todo>>) -> Option<usize> {
+    state.iter().position(|model| model.id == id)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app_data = AppData {
-        todos: vec![] as Vec<Todo>,
-    };
+    let app_data = Data::new(AppData {
+        todos: Mutex::new(vec![]) as Mutex<Vec<Todo>>,
+        current_id: Mutex::new(0),
+    });
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(app_data.todos.clone()))
+            .app_data(app_data.clone())
             .service(index)
+            .service(insert)
+            .service(update)
+            .service(remove)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
