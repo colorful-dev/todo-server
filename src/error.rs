f31e88a424a862d70ae16cd52f1e92a92b4fe8fb
SunLee <hildxd@outlook.com>
2022-11-12 03:06:28 +0800

    feat: custom reponse

---
+++ src/error.rs
@@ -0,0 +1,73 @@
+use axum::{
+    response::IntoResponse,
+    Json,
+};
+
+use crate::Response;
+
+/// 错误的类型
+pub enum AppErrorType {
+    /// 无错误
+    Ok,
+    /// 数据库错误
+    DbError,
+    /// 未找到
+    NotFound,
+}
+
+/// 应用错误
+pub struct AppError {
+    /// 错误信息
+    pub message: Option<String>,
+    /// 错误原因（上一级的错误）
+    pub cause: Option<String>,
+    /// 错误类型
+    pub error_type: AppErrorType,
+}
+
+impl AppError {
+    fn code(&self) -> i32 {
+        match self.error_type {
+            AppErrorType::Ok => 0,
+            AppErrorType::DbError => 1,
+            AppErrorType::NotFound => 2,
+        }
+    }
+    /// 从上级错误中创建应用错误
+    fn from_err(err: impl ToString, error_type: AppErrorType) -> Self {
+        Self {
+            message: None,
+            cause: Some(err.to_string()),
+            error_type,
+        }
+    }
+    /// 从字符串创建应用错误
+    fn from_str(msg: &str, error_type: AppErrorType) -> Self {
+        Self {
+            message: Some(msg.to_string()),
+            cause: None,
+            error_type,
+        }
+    }
+    /// 数据库错误
+    pub fn db_error(err: impl ToString) -> Self {
+        Self::from_err(err, AppErrorType::DbError)
+    }
+    /// 未找到
+    pub fn not_found() -> Self {
+        Self::from_str("不存在的记录", AppErrorType::NotFound)
+    }
+}
+
+/// 实现 IntoResponse
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        let code = self.code();
+        let msg = match self.message {
+            Some(msg) => msg,
+            None => "有错误发生".to_string(),
+        };
+        let res: Response<()> = Response::err(code, msg);
+        Json(res).into_response()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use axum::{routing::get, Router};
 use dotenv::dotenv;
-mod config;
 
-fn main() {
+mod config;
+mod handler;
+mod error;
+mod response;
+pub use response::Response;
+
+/// 定义自己的 Result
+type Result<T> = std::result::Result<T, error::AppError>;
+
+#[tokio::main]
+async fn main() {
     // 解析 .env文件
     dotenv().ok();
     // 初始化配置
     let cfg = config::Config::from_env().expect("初始化配置失败");
-    println!("{:?}", cfg);
+    // 路由
+    let app = Router::new().route("/", get(handler::usage));
+
+    axum::Server::bind(&cfg.web.addr.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
